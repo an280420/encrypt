3d8ec1b9edc4ceedd2482562a13d75f59e24cee8
@an280420 <angit280420@gmail.com>
2020-11-12 21:22:58 +0500

    Added method SHA2

--- encrypt.rb
+++ encrypt.rb
@@ -6,11 +6,12 @@ text = gets.chomp
 puts "Каким способом зашифровать"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 
 encrypt_method = gets.to_i
 
-until encrypt_method.between?(1, 2)
-  puts "Выберите 1 или 2"
+until encrypt_method.between?(1..3)
+  puts "Выберите 1, 2 или 3"
   encrypt_method = gets.to_i
 end
 
@@ -18,6 +19,9 @@ puts "Вот что получилось"
 
 if encrypt_method == 1
   puts Digest::MD5.hexdigest text
-else
+elsif encrypt_method == 2
   puts Digest::SHA1.hexdigest text
+else
+  puts Digest::SHA2.hexdigest text
 end
+
